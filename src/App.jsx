701d6f572b4028bc51c629f63d7b5b8971b3bd3f
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-03-13 09:19:55 +0500

    Add flowbite-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { Button } from "flowbite-react";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Button>Click me</Button>;
 }
 
 export default App;
