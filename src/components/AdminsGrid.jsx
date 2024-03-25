50672e34e2d047d9bef0d6a114325a7c526d00dc
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-03-25 18:03:00 +0500

    Add admin grids

---
+++ src/components/AdminsGrid.jsx
@@ -0,0 +1,83 @@
+import { Button, Card, Spinner, Tooltip } from "flowbite-react";
+import { FaInfoCircle } from "react-icons/fa";
+import { IoMdArchive } from "react-icons/io";
+import { MdDelete, MdOutlineEdit, MdUnarchive } from "react-icons/md";
+import AdminAvatar from "/assets/admin.png";
+import { useTranslation } from "react-i18next";
+
+function AdminsGrid({ admins, loading }) {
+  const { t } = useTranslation();
+
+  function handleDelete(phoneNumber) {
+    dispatch(deleteAdmin({ phoneNumber }));
+  }
+
+  return (
+    <>
+      {!loading && admins && (
+        <ul className="grid grid-cols-4 gap-5">
+          {admins.map(({ id, firstName, phoneNumber, isActive }) => {
+            return (
+              <li key={id}>
+                <Card>
+                  <div className="flex flex-col items-center py-5">
+                    <img
+                      alt={firstName}
+                      height="96"
+                      src={AdminAvatar}
+                      width="96"
+                      className="mb-3 rounded-full shadow-lg"
+                    />
+                    <h5 className="mb-1 line-clamp-1 text-xl font-medium text-gray-900 dark:text-white">
+                      {firstName}
+                    </h5>
+                    <span className="text-sm text-gray-500 dark:text-gray-400">
+                      {phoneNumber}
+                    </span>
+                    <div className="mt-4 flex space-x-3 lg:mt-6">
+                      <Tooltip content={t(isActive ? "archive" : "unarchive")}>
+                        <Button
+                          onClick={() => handleDelete(phoneNumber)}
+                          color={isActive ? "warning" : "success"}
+                          size="sm"
+                        >
+                          {isActive ? <IoMdArchive /> : <MdUnarchive />}
+                        </Button>
+                      </Tooltip>
+                      <Tooltip content={t("info")}>
+                        <Button
+                          onClick={() => handleDelete(phoneNumber)}
+                          color="purple"
+                          size="sm"
+                        >
+                          <FaInfoCircle />
+                        </Button>
+                      </Tooltip>
+                      <Tooltip content={t("delete")}>
+                        <Button
+                          onClick={() => handleDelete(phoneNumber)}
+                          color="failure"
+                          size="sm"
+                        >
+                          <MdDelete />
+                        </Button>
+                      </Tooltip>
+                    </div>
+                  </div>
+                </Card>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+      {loading && (
+        <div className="absolute left-2/4 top-24 flex -translate-x-2/4 select-none items-center gap-5">
+          <Spinner />
+          <span>{t("loading")}</span>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default AdminsGrid;
--- src/pages/Admins.jsx
+++ src/pages/Admins.jsx
@@ -1,60 +1,37 @@
-import { Button, Card, Dropdown } from "flowbite-react";
+import { Button, Card, Dropdown, Tooltip } from "flowbite-react";
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { FaPlus } from "react-icons/fa6";
 import { useDispatch, useSelector } from "react-redux";
-import { getAdmins } from "../redux/slices/admins";
-import AdminAvatar from "/assets/admin.png";
+import { deleteAdmin, getAdmins } from "../redux/slices/admins";
+import { modalManager } from "../redux/slices/modals";
+import AdminsGrid from "../components/AdminsGrid";
 
 export default function Admins() {
   const { t } = useTranslation();
-  const { data } = useSelector((state) => state.adminsSlice);
+  const { data, loading } = useSelector((state) => state.adminsSlice);
   const dispatch = useDispatch();
+
+  function handleModal() {
+    dispatch(modalManager("addAdminModal"));
+  }
 
   useEffect(() => {
     dispatch(getAdmins());
-    console.log(data);
   }, []);
 
   return (
     <section className="flex flex-col">
       <div className="mb-5 flex justify-between">
         <h2 className="text-2xl font-bold">{t("admins")}</h2>
-        <Button color="dark">
+        <Button onClick={handleModal} color="dark">
           <FaPlus className="mr-2 h-5 w-5" />
-          Admin qo'shish
+          {t("addAdmin")}
         </Button>
       </div>
-      <ul className="grid grid-cols-3 gap-5">
-        {data?.map((admin) => {
-          return (
-            <li>
-              <Card className="max-w-sm">
-                <div className="flex justify-end px-4 pt-4"></div>
-                <div className="flex flex-col items-center pb-10">
-                  <img
-                    alt="Bonnie image"
-                    height="96"
-                    src={AdminAvatar}
-                    width="96"
-                    className="mb-3 rounded-full shadow-lg"
-                  />
-                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                    {admin.firstName}
-                  </h5>
-                  <span className="text-sm text-gray-500 dark:text-gray-400">
-                    {admin.phoneNumber}
-                  </span>
-                  <div className="mt-4 flex space-x-3 lg:mt-6">
-                    <Button color="failure">{t("delete")}</Button>
-                    <Button color="dark">Ma'lumot olish</Button>
-                  </div>
-                </div>
-              </Card>
-            </li>
-          );
-        })}
-      </ul>
+      <div className="relative">
+        <AdminsGrid admins={data} loading={loading} />
+      </div>
     </section>
   );
 }
